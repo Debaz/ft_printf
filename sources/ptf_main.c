0807374066bc60158738b42f7ed4172a21677987
Kantin LESCAUDRON <klescaud@e1r3p11.42.fr>
2016-02-16 15:33:00 +0100

    Push Debaz

--- sources/ptf_main.c
+++ sources/ptf_main.c
@@ -1,15 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ptf_main.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: klescaud <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/16 14:47:45 by klescaud          #+#    #+#             */
-/*   Updated: 2016/02/16 14:47:57 by klescaud         ###   ########.fr       */
+/*   Updated: 2016/02/16 15:32:40 by klescaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <ft_printf.h>
+
 /*
-** VOID
+** Temporary test function
 */
+
+int		main(void)
+{
+	int		ptfreturn;
+
+	ptfreturn = ft_printf("My name is %s.\n", "Slim Shady");
+	ft_putstr("ft_printf return value = ");
+	ft_putnbr(ptfreturn);
+	ft_putendl("");
+	ft_putendl("-------------------------------------------------");
+	ptfreturn = printf("My name is %s.\n", "Slim Shady");
+	ft_putstr("printf return value = ");
+	ft_putnbr(ptfreturn);
+	ft_putendl("");
+	return (0);
+}
+
+int		ft_printf(char *format, ...)
+{
+	//WIP
+}
